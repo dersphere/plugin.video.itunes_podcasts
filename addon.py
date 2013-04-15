4412ea8425d5ecfe7b0451c08cdf89082efc93eb
dersphere <sphere@dersphere.de>
2013-04-15 21:52:37 +0200

    double encode some podcasts

--- addon.py
+++ addon.py
@@ -239,7 +239,7 @@ def __add_podcast_items(content_type, podcast_id, podcast_items):
             endpoint='watch_item',
             content_type=content_type,
             podcast_id=podcast_id,
-            item_url=item['item_url']
+            item_url=item['item_url'].encode('utf-8')
         ),
         'is_playable': True
     } for i, item in enumerate(podcast_items)]
